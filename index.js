b54486bf6fad4cafc15ac079188b6ad6aa3c40ac
Philippe Grandmont <pgrandmont@atmanco.com>
2016-02-03 09:23:56 -0500

    port error fix

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ var Convo = require('./src/Convo.js');
 var AtmanWrapper = require('./src/AtmanWrapper');
 var UsersList = require('./src/UsersList.js');
 var MessageFormatter = require('./src/MsgFormatter.js');
+var express = require('express');
+
+
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+	var result = 'App is running'
+	response.send(result);
+}).listen(app.get('port'), function() {
+	console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 // Get Server Config
 var botToken = config.get('botToken');
